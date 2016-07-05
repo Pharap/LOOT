051d32d4ab71951d1ee67129dbf7dcb354fb21c2
Pharap <pharap@hotmail.com>
2016-07-05 12:41:07 +0100

    Added BitArray template

--- loot/bitarray.h
+++ loot/bitarray.h
@@ -93,3 +93,44 @@ public:
         return this->value;
     }
 };
+
+template <uint8_t Size>
+class BitArray
+{
+private:
+	static_assert(Size < 255, "Size cannot exceed 254 elements");
+	const uint8_t Count = (Size / 8) + (((Size % 8) > 0) ? 1 : 0);
+
+private:
+	BitArray8 values[(Size / 8) + (((Size % 8) > 0) ? 1 : 0)];
+
+public:
+	BitArray(void) : values()
+	{
+	}
+
+	uint8_t getSize(void) const
+	{
+		return Size;
+	}
+
+	bool get(const uint8_t index) const
+	{
+		return this->values[index / 8].get(index % 8);
+	}
+
+	void set(const uint8_t index, const bool value)
+	{
+		return this->values[index / 8].set(index % 8, value);
+	}
+
+	void set(const uint8_t index)
+	{
+		return this->values[index / 8].set(index % 8);
+	}
+
+	void clear(const uint8_t index)
+	{
+		return this->values[index / 8].clear(index % 8);
+	}
+};
