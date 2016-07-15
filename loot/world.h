b7e96188f794b4db9356c3a347cafc6cac50300b
noelabercrombie <noel.abercrombie@hotmail.com>
2016-07-15 01:30:35 +0100

    merge fixes

--- loot/world.h
+++ loot/world.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdint.h>
 #include "list.h"
-#include "itemtype.h"
+#include "Itemtype.h"
 
 // Chest needs its own file
 class Chest
@@ -17,8 +17,7 @@ private:
 public:
     Chest(void) = default;
     Chest(const int8_t x, const int8_t y, const ItemType type); 
-    void setType(const ItemType type); // I find the presence of only 1 set odd, but oh well
-    void setPos(const int8_t x, const int8_t y);    //boom, there you go
+    void setType(ItemType type); // I find the presence of only 1 set odd, but oh well
     ItemType getType(void) const;
     int8_t getX(void) const;
     int8_t getY(void) const;
@@ -28,6 +27,8 @@ using ChestList = List<Chest, 16>;
 
 class World
 {
+  private:
+    uint8_t battleTendency;
   public:
     uint8_t width,height;
     uint8_t level[128]; //biggest map size, 16*16, 32*8, etc
@@ -37,6 +38,8 @@ class World
 
     void init(void);
     void step(void);
+
+    //map stuff
     void load(uint8_t *ID);
     void set(const int8_t x, const int8_t y, const uint8_t wall);
     void setFast(const int8_t x, const int8_t y, const uint8_t wall);
@@ -45,12 +48,14 @@ class World
     uint8_t getFast(const int8_t x, const int8_t y) const;
     bool inbound(const int8_t x, const int8_t y) const;
 
-    //stupid
+    //battle stuff
+    uint8_t getBattleChance(void);
+
+    //item stuff
     void setItem(const uint8_t item, const int8_t x, const int8_t y, const ItemType type);
     ItemType getItemType(const int8_t x, const int8_t y);
     uint8_t getItemID(const int8_t x, const int8_t y);
     // This should be 'hasItem',
     // 'getItem' implies you can actually retrieve a reference to said item
     bool hasItem(const int8_t x, const int8_t y);
-    //stupid
 };
