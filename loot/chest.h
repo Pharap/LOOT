7bb0283674a9555bea3897183c1ad7cbbab1c3ad
Pharap <pharap@hotmail.com>
2016-07-17 07:53:32 +0100

    Move functionality into chest.h
    Moving code into chest.cpp preventing crucial inlining, resulting in a 272 byte increase.
    Moving that code into chest.h instead maintained inlining, resulting in no change.

--- loot/chest.h
+++ loot/chest.h
@@ -11,13 +11,42 @@ private:
 
 public:
     Chest(void) = default;
-    Chest(const int8_t x, const int8_t y, const ItemType type);
-    ItemType getType(void) const;
-    void setType(const ItemType type);
-    int8_t getX(void) const;
-    void setX(const int8_t x);
-    int8_t getY(void) const;
-    void setY(const int8_t y);
+    Chest(const int8_t x, const int8_t y, const ItemType type)
+    {
+      this->x = x;
+      this->y = y;
+      this->type = type;
+    }
+    
+    ItemType getType(void) const
+    {
+      return type;
+    }
+    
+    void setType(const ItemType type)
+    {
+      this->type = type;
+    }
+    
+    int8_t getX(void) const
+    {
+      return x;
+    }
+    
+    void setX(const int8_t x)
+    {
+      this->x = x;
+    }
+    
+    int8_t getY(void) const
+    {
+      return y;
+    }
+    
+    void setY(const int8_t y)
+    {
+      this->y = y;
+    }
 };
 
 using ChestList = List<Chest, 16>;
